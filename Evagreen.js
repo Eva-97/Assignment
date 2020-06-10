79521c3a7e1ebd3ed1f136323dee2941d4ab3d58
Eva-97 <65343553+Eva-97@users.noreply.github.com>
2020-06-10 20:33:01 +0100

    Create Evagreen.js
    This is a group of codes showing a prompt on data for my clients.

---
+++ Evagreen.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+var data={
+name:prompt("Name:"), 
+state:prompt("State of Origin:"), 
+number:prompt("Phone Number:"), 
+eyeColor:prompt("Eye Colour:"), 
+height:prompt("Your height:"), 
+weight:prompt("Your weight:"), 
+} 
+var person = data
+console.log(person)
+
+</script>
